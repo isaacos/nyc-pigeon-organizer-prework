3f6c0f6fc5ff995b3a5c0bd5bb4d9ab7143d2a5a
Isaac Oseas <oseasisaac91@gmail.com>
2018-10-24 05:09:59 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
-def nyc_pigeon_organizer(data)
+
+def nyc_pigeon_organizer(pigeon_data)
   # write your code here!
+  pigeon_list = {}
+
+  pigeon_data.each do |color_gender_lives, facts|
+    facts.each do |facts_key, array_names|
+    
+    
+      array_names.each do |names|
+      pigeon_list[names] = {}
+
+      end
+
+    end
+
+  end
+
+  pigeon_data.each do |color_gender_lives, facts|
+
+  pigeon_list.keys.each do |pigeon_name|
+
+  pigeon_list[pigeon_name][color_gender_lives] = []
+  end
+
+  end
+
+
+pigeon_list
+
+
 end
